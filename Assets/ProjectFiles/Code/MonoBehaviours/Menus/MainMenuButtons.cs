1a5a6dae080082e35bc3661ead29c86c9664ce1c
Lubrisa <luigilubrisacampregher@gmail.com>
2023-10-17 23:47:50 -0300

    Funcionalidades Main Menu

--- Assets/ProjectFiles/Code/MonoBehaviours/Menus/MainMenuButtons.cs
+++ Assets/ProjectFiles/Code/MonoBehaviours/Menus/MainMenuButtons.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace PointnClick
 {
     public class MainMenuButtons : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        public void StartGame() => SceneManager.LoadScene(1);
 
-        // Update is called once per frame
-        void Update()
+        public void Credits() => SceneManager.LoadScene(6);
+
+        public void Exit()
         {
-        
+            Application.Quit();
+
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#endif
         }
     }
 }
